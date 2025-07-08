fda019757592b2cbf5882c7b502a921c0f567282
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-07-08 21:30:34 +0530

    Refactor App component to include Header and Footer for improved layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-    <h1>Welcome to LenseMIMI</h1>
-    </div>
+    <>
+      <Header />
+      <main>
+        <h1>Welcome to LenseMIMI</h1>
+      </main>
+      <Footer />
+    </>
   );
 }
 
