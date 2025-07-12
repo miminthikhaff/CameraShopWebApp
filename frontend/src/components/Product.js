a53ecc8e5a8689fa0f31b016bf15655a37189285
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-07-12 18:06:14 +0530

    feat: add Product component to display product details with Bootstrap styling

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+const Product = ({ product }) => {
+    return (
+        <Card className="my-3 p-3 rounded">
+            <a href={`/product/${product._id}`}>
+                <Card.Img src={product.image} variant="top" />
+            </a>
+
+            <Card.Body>
+                <a href={`/product/${product._id}`}>
+                    <Card.Title as="div">
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </a>
+
+                <Card.Text as="div">
+                    <div className="my-3">
+                        {product.rating} from {product.numReviews} reviews
+                    </div>
+                </Card.Text>
+
+                <Card.Text as="h3">${product.price}</Card.Text>
+                
+                <Card.Text as="div">
+                    <div className="my-3">
+                        {product.countInStock > 0 ? (
+                            <span className="text-success">In Stock</span>
+                        ) : (
+                            <span className="text-danger">Out of Stock</span>
+                        )}
+                    </div>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Product
