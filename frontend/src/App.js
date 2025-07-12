57b118348684e32c9785134932ea3ce55f7c949e
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-07-12 18:05:48 +0530

    refactor: restructure App component to include HomeScreen within a Container for better layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { Container } from 'react-bootstrap'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <>
       <Header />
-      <main>
-        <h1>Welcome to PixelLense</h1>
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
       </main>
       <Footer />
     </>
