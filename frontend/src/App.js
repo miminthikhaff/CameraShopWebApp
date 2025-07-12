b1841a3b75f94a501fadeeed47e2977b764234b1
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-07-12 23:58:21 +0530

    refactor: remove duplicate Bootstrap CSS import in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
