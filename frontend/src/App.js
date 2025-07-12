c4f64f978b5a136c29c77655d73e1be8f253142d
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-07-12 23:17:59 +0530

    refactor: update routing structure to use Routes and element prop for improved readability

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap'
+import { Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <Router>
-
+    <>
       <Header />
       <main className="py-3">
         <Container>
-          <Route path='/' Component={HomeScreen} exact />
-          <Route path='/product/:id' Component={ProductScreen} />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-
-    </Router>
+    </>
   );
 }
 
